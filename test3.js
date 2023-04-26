3b043d800302cae05cc55bdd7983a98079718c74
levi87 <levi87@126.com>
2023-04-26 09:57:18 +0800

    Create test3.js

---
+++ test3.js
@@ -0,0 +1,26 @@
+const url = $persistentStore.read("jurl");
+
+$httpClient.get(url, function(error, response, data) {
+    if (error) {
+        console.log(error);
+        $done();
+        return;
+    }
+    
+    const result = JSON.parse(data);
+    const bwUsed = result.used;
+    
+    const today = new Date();
+    let month = today.getMonth() + 2;
+    if (month > 12) {
+        month = 1;
+    }
+    
+    const panel = {
+        title: "✈️ 🍺 𝙄𝙣𝙛𝙤",
+        content: `已使用流量：${(bwUsed)} GB\n下次重置日期：${month}月1号`,
+        icon: ''
+    };
+    
+    $done(panel);
+});
