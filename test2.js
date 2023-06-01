fbbaa88fdde3e5f53189121e4e4586c281441b1d
levi87 <levi87@126.com>
2023-06-01 13:31:50 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,6 +1,7 @@
 !(async () => {
-let params = getParams($argument);
-$httpClient.get(params.url, function(error, response, data) {
+let url = getParams($argument);
+let name = getParams($argument2);
+$httpClient.get(url, function(error, response, data) {
     if (error) {
         console.log(error);
         $done();
@@ -19,7 +20,7 @@ $httpClient.get(params.url, function(error, response, data) {
     }
     
     const panel = {
-        title: params.name || 'Server Info',
+        title: name || 'Server Info',
         content: `已使用流量：${(bwUsed / 1000000000).toFixed(3)} GB\n流量剩余：${((bwLimit - bwUsed) / 1000000000).toFixed(3)} GB\n下次重置日期：${month}月${bwResetDay}号`,
         icon: 'checkmark.seal'
     };
