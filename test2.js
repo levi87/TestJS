290dd9f106e3d25ca690ece30cd667c3e4ba4ba2
levi87 <541867+levi87@users.noreply.github.com>
2023-08-14 10:12:45 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -13,6 +13,7 @@ $httpClient.get(params.url, function(error, response, data) {
     const bwResetDay = result.bw_reset_day_of_month;
     const lineType = ['𝘾𝙈𝙄 ', '𝘾𝙈𝙄+𝙉𝙏𝙏 ', '𝙄𝙋𝙇𝘾 ', '', '𝙂𝙄𝘼', '𝙂𝙄𝘼-𝙀'];
     const crType = ['𝘾𝙉 ', '𝙃𝙆 ', '𝙐𝙎 ', '𝙇𝘼 ', '𝙅𝙋 ', '𝙎𝙂 ', '𝙐𝙆 ', '𝙏𝙒 ', '𝙆𝙍 ', '𝙇𝙊𝙉𝘿𝙊𝙉 ', ''];
+    const verType = ['𝙑1 ' ,'𝙑2 ' ,'𝙑3 ' , ''];
     
     const today = new Date();
     var month = 0;
@@ -26,7 +27,7 @@ $httpClient.get(params.url, function(error, response, data) {
     }
     
     const panel = {
-        title: `${params.name}${crType[params.crnum]}${lineType[params.linenum]}𝙉𝙤𝙙𝙚 𝙎𝙚𝙧𝙫𝙚𝙧 𝙄𝙣𝙛𝙤`,
+        title: `${params.name}${crType[params.crnum]}${lineType[params.linenum]}${verType[params.vernum]}𝙉𝙤𝙙𝙚 𝙎𝙚𝙧𝙫𝙚𝙧 𝙄𝙣𝙛𝙤`,
         content: `已用流量：${(bwUsed / 1000000000).toFixed(3)} GB\n剩余流量：${((bwLimit - bwUsed) / 1000000000).toFixed(3)} GB\n重置日期：${month}月${bwResetDay}号`,
         icon: 'checkmark.seal'
     };
