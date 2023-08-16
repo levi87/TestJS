c8889da8d46518804542c7d30685874592cc15fa
levi87 <541867+levi87@users.noreply.github.com>
2023-08-16 15:02:51 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -28,7 +28,7 @@ $httpClient.get(params.url, function(error, response, data) {
     
     const panel = {
         title: `${params.name}${crType[params.crnum]}${lineType[params.linenum]}${verType[params.vernum]}𝙉𝙤𝙙𝙚 𝙎𝙚𝙧𝙫𝙚𝙧 𝙄𝙣𝙛𝙤`,
-        content: `已用流量：${(bwUsed / 1000000000).toFixed(3)} GB\n剩余流量：${((bwLimit - bwUsed) / 1000000000).toFixed(3)} GB\n重置日期：${month}月${bwResetDay}号`,
+        content: `已用：${(bwUsed / 1000000000).toFixed(3)} GB | 剩余：${((bwLimit - bwUsed) / 1000000000).toFixed(3)} GB | 重置：${month}月${bwResetDay}号`,
         icon: 'externaldrive.badge.icloud'
     };
     
