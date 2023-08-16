0d2f4cfea9cc1e2e397e387f7e46e08f6bd27a88
levi87 <541867+levi87@users.noreply.github.com>
2023-08-16 15:12:26 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -28,8 +28,8 @@ $httpClient.get(params.url, function(error, response, data) {
     
     const panel = {
         title: `${params.name}${crType[params.crnum]}${lineType[params.linenum]}${verType[params.vernum]}𝙉𝙤𝙙𝙚 𝙎𝙚𝙧𝙫𝙚𝙧 𝙄𝙣𝙛𝙤`,
-        content: `已用：${(bwUsed / 1000000000).toFixed(3)} GB | 剩余：${((bwLimit - bwUsed) / 1000000000).toFixed(3)} GB | 重置：${month}月${bwResetDay}号`,
-        icon: 'externaldrive.badge.icloud'
+        content: `已用:${(bwUsed / 1000000000).toFixed(3)} GB | 剩余:${((bwLimit - bwUsed) / 1000000000).toFixed(3)} GB | 重置:${month}月${bwResetDay}号`,
+        icon: 'server.rack'
     };
     
     $done(panel);
