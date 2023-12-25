53eee81d8ffe0e264d070982912500d57d8b2522
levi87 <541867+levi87@users.noreply.github.com>
2023-12-25 09:16:13 +0800

    更新 test2.js

--- test2.js
+++ test2.js
@@ -29,7 +29,7 @@ $httpClient.get(params.url, function(error, response, data) {
     const panel = {
         title: `${params.name}${crType[params.crnum]}${lineType[params.linenum]}${verType[params.vernum]}𝙉𝙤𝙙𝙚 𝙎𝙚𝙧𝙫𝙚𝙧 𝙄𝙣𝙛𝙤`,
         content: `🆄${(bwUsed / 1000000000).toFixed(3)} GB | 🆁${((bwLimit - bwUsed) / 1000000000).toFixed(3)} GB | ♼${month}-${bwResetDay}`,
-        icon: 'server.rack'
+        icon: 'network.badge.shield.half.filled'
     };
     
     $done(panel);
